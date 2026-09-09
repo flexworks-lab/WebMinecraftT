c2e05e161c40c40a91a44e75805cc5e0f640c988
Net_tech <worthmarcus19@gmail.com>
2026-09-09 19:31:23 -0400

    Restore moving menu background and limit dirt to world selection

--- src/background.js
+++ src/background.js
@@ -3,31 +3,12 @@ function applyDirtBackgrounds() {
     const style = document.createElement("style");
     style.id = "webMinecraftDirtBackgrounds";
     style.textContent = `
-html, body {
-    background:#6b4728 url("./textures/dirt.png") repeat;
-    background-size:64px 64px;
-}
-#mainMenu,
-#seedMenu,
-#settingsMenu,
 #savedWorlds {
     background-color:rgba(35,24,16,.72) !important;
     background-image:url("./textures/dirt.png") !important;
     background-repeat:repeat !important;
     background-size:64px 64px !important;
 }
-#mainMenu::before {
-    background:linear-gradient(rgba(0,0,0,.28),rgba(0,0,0,.5)),url("./textures/dirt.png") repeat !important;
-    background-size:64px 64px !important;
-    opacity:.92;
-}
-#mainMenu::after {
-    background:rgba(0,0,0,.18) !important;
-    backdrop-filter:blur(1px);
-    -webkit-backdrop-filter:blur(1px);
-}
-#seedPanel,
-#settingsPanel,
 #savedWorldsShell {
     background-color:rgba(28,20,14,.82) !important;
     background-image:linear-gradient(rgba(20,14,10,.62),rgba(20,14,10,.82)),url("./textures/dirt.png") !important;
