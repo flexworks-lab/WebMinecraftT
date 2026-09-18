eaf8469f20897608a57d53b4f3c1c8e2270c9c37
Net_tech <worthmarcus19@gmail.com>
2026-09-18 01:41:20 -0400

    Remove dirt texture from worlds UI

--- src/background.js
+++ src/background.js
@@ -9,28 +9,6 @@ import "./waterTextureFix.js";
 import "./survivalMode.js";
 import "./performance.js";
 
-function applyDirtBackgrounds() {
-    if (document.getElementById("webMinecraftDirtBackgrounds")) return;
-    const style = document.createElement("style");
-    style.id = "webMinecraftDirtBackgrounds";
-    style.textContent = `
-#savedWorlds {
-    background-color:rgba(35,24,16,.72) !important;
-    background-image:url("./textures/dirt.png") !important;
-    background-repeat:repeat !important;
-    background-size:64px 64px !important;
-}
-#savedWorldsShell {
-    background-color:rgba(28,20,14,.82) !important;
-    background-image:linear-gradient(rgba(20,14,10,.62),rgba(20,14,10,.82)),url("./textures/dirt.png") !important;
-    background-repeat:repeat !important;
-    background-size:64px 64px !important;
-}
-#savedWorldsBody { background:rgba(0,0,0,.08); }
-`;
-    document.head.appendChild(style);
-}
-
 function setupMenuAndMobileUi() {
     if (document.getElementById("webMinecraftMenuUiFixes")) return;
     const style = document.createElement("style");
