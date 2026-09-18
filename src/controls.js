474168cb2d59dfdc000abcb4f0b6ab50ea04c6f6
Net_tech <worthmarcus19@gmail.com>
2026-09-17 21:38:34 -0400

    Require a new jump press for each jump

--- src/controls.js
+++ src/controls.js
@@ -288,7 +288,7 @@ export function setupControls() {
         }
         if (event.code === "KeyF" && !event.repeat) isFlying = !isFlying;
         keys[event.code] = true;
-        if (event.code === "Space") jumpQueued = true;
+        if (event.code === "Space" && !event.repeat) jumpQueued = true;
         if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.code)) event.preventDefault();
     });
     window.addEventListener("keyup", event => {
